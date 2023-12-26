867bb7ed767af831c07b187fbaa6e90d8e25d4c7
aryamanpandya99 <aryaman.pandya@tufts.edu>
2023-12-26 00:08:36 -0500

    modified:   models/modules.py 	modified:   models/transformer_blocks.py 	modified:   notebooks/encoder_only_classification.ipynb

--- models/modules.py
+++ models/modules.py
@@ -1,10 +1,13 @@
+"""
+Implementation of all the sub-modules required to build transformer blocks
+"""
 import torch
 import math
 from torch import nn
 
 def scaled_dot_product_attention(q, k, d_k, mask):
     scores = torch.matmul(q, k.transpose(-2, -1)) / math.sqrt(d_k)
-    if mask is not None:
+    if mask:
         scores = scores.masked_fill(mask == 0, float('-inf'))
     return nn.Softmax(-1)(scores)
 
@@ -22,6 +25,10 @@ def positional_embedding(input_tensor: torch.Tensor, output_dim: int, n=10000):
 
 
 class MultiHeadAttention(nn.Module):
+    """
+    Multihead attention class implementation. Can act as self-attention (default, y is None)
+    or cross attention if y not none
+    """
     def __init__(self, d_k, d_model, d_v, dropout, num_heads, mask) -> None:
         super(MultiHeadAttention, self).__init__()
         self.d_k, self.d_v, self.d_model, self.num_heads = d_k, d_v, d_model, num_heads
@@ -31,27 +38,37 @@ class MultiHeadAttention(nn.Module):
         self.dropout = nn.Dropout(dropout)
         self.mask = mask
 
-    def forward(self, q, k, v):
+    def forward(self, x, y = None):
+        residual = x
+        x = self.layer_norm(x)
+        if y is not None:
+            k, q, v = y, x, y
+        else:
+            k, q, v = x, x, x
+        
         k_len, q_len, v_len, batch_size = k.size(1), q.size(1), v.size(1),  q.size(0)
-        residual = q
         k = self.key_layer(k).view(batch_size, k_len,  self.num_heads, self.d_k)
         q = self.query_layer(q).view(batch_size, q_len,  self.num_heads, self.d_k)
         v = self.value_layer(v).view(batch_size, v_len,  self.num_heads, self.d_v)
-        q, k, v = q.transpose(1, 2), k.transpose(1, 2), v.transpose(1, 2)
-        attention = scaled_dot_product_attention(q, k, self.d_k, self.mask)
-        output = torch.matmul(attention, v)
+        attention = scaled_dot_product_attention(q.transpose(1, 2), k.transpose(1, 2), self.d_k, self.mask)
+        output = torch.matmul(attention, v.transpose(1, 2))
         output = self.concat_projection(output.transpose(1, 2).contiguous().view(batch_size, q_len, -1))
-        return self.dropout(self.layer_norm(output+residual))
+        
+        return self.dropout(output) + residual
 
 class PositionWiseFFN(nn.Module): 
+    """
+    Class implementation of the position wise MLP
+    """
     def __init__(self, d_model, d_ff, dropout) -> None:
         super(PositionWiseFFN, self).__init__()
-        self.fc1 = nn.Sequential(nn.Linear(d_model, d_ff, bias=True),nn.ReLU())
+        self.fc1 = nn.Sequential(nn.Linear(d_model, d_ff, bias=True), nn.ReLU())
         self.fc2 = nn.Linear(d_ff, d_model, bias=True)
         self.layer_norm = nn.LayerNorm(d_model)
         self.dropout = nn.Dropout(dropout)
     
     def forward(self, x):
         residual = x
-        x = self.fc2(self.fc1(x))        
-        return self.dropout(self.layer_norm(x+residual))
+        x = self.layer_norm(x)  # layer norm before MLP, deviation from paper now common practics
+        x = self.fc2(self.fc1(x))
+        return self.dropout(x) + residual
--- models/transformer_blocks.py
+++ models/transformer_blocks.py
@@ -1,23 +1,29 @@
+"""
+Implementations of different versions of transformer layers/blocks
+Aryaman Pandya
+"""
+
 from modules import MultiHeadAttention, PositionWiseFFN, positional_embedding
 from torch import nn
 
-class TransformerLayer(nn.Module): 
+class TransformerLayer(nn.Module):
+    """
+    Class implementation of a generic single self-attention only transformer layer
+    """
     def __init__(self, d_k, d_model, d_v, num_heads, d_ff, mask, dropout) -> None:
         super(TransformerLayer, self).__init__()
-        self.k_layer, self.q_layer, self.v_layer = nn.Linear(d_model, d_model), nn.Linear(d_model, d_model), nn.Linear(d_model, d_model)
         self.multihead_attention = MultiHeadAttention(d_k, d_model, d_v, dropout, num_heads, mask)
         self.pointwise_ffn = PositionWiseFFN(d_model, d_ff, dropout)
 
-    def forward(self, x): 
-        k, q, v = self.k_layer(x), self.q_layer(x), self.v_layer(x)
-        output = self.multihead_attention(q, k, v)
+    def forward(self, x):
+        output = self.multihead_attention(x)
         return self.pointwise_ffn(output)
 
 
 class Transformer(nn.Module):
     """
     If mask = False, this is an Encoder and if mask = True
-    this is a decoder for a decoder only transformer
+    this is a decoder for a decoder only transformer (no cross attention)
     """
     def __init__(self, d_k, d_model, d_v, d_ff, num_heads, num_layers, vocab_size, mask=False, dropout=0.1) -> None:
         super(Transformer, self).__init__()
@@ -29,35 +35,31 @@ class Transformer(nn.Module):
 
     def forward(self, x):
         embedded = self.embedding(x)
-        x = self.dropout(embedded + self.positional_embedding(x, self.d_model))
+        x = self.dropout(embedded + self.positional_embedding(x, self.d_model).to(x.device))
         for layer in self.layers:
+            layer = layer.to(x.device)
             x = layer(x)
         return x
 
 class DecoderLayer(nn.Module):
     """
-    This module is designed as per the Attention is All You Need paper,
-    assuming that we're propagating some output from the encoder to the decoder.
-    For decoder only models, we can get rid of the cross attention module
-    and the only difference between the encoder and decoder in this case
-    would be the masking of future tokens
+    Implementation of a decoder layer containing a cross attention layer
     """
     def __init__(self, d_k, d_model, d_v, num_heads, d_ff, dropout) -> None:
         super(DecoderLayer, self).__init__()
-        self.k_layer, self.q_layer, self.v_layer = nn.Linear(d_model, d_model), nn.Linear(d_model, d_model), nn.Linear(d_model, d_model)
-        self.k_layer2, self.q_layer2, self.v_layer2 = nn.Linear(d_model, d_model), nn.Linear(d_model, d_model), nn.Linear(d_model, d_model)
         self.masked_multihead_attention = MultiHeadAttention(d_k, d_model, d_v, dropout, num_heads, mask=True)
         self.multihead_attention = MultiHeadAttention(d_k, d_model, d_v, dropout, num_heads, mask=False)
         self.positiontwise_ffn = PositionWiseFFN(d_model, d_ff, dropout)
 
     def forward(self, outputs, encoder_output):
-        k1, q1, v1 = self.k_layer(outputs), self.q_layer(outputs), self.v_layer(outputs)
-        outputs = self.masked_multihead_attention(q1, k1, v1, True)
-        k2, q2, v2 = self.k_layer2(encoder_output), self.q_layer2(outputs), self.v_layer2(encoder_output)  # query comes from outputs of previous decoder layer while k, v come from the encoder's output
-        outputs = self.multihead_attention(q2, k2, v2)
+        outputs = self.masked_multihead_attention(outputs)  # self attention
+        outputs = self.multihead_attention(outputs, encoder_output)  # cross attention
         return self.positiontwise_ffn(outputs)
     
 class Decoder(nn.Module):
+    """
+    Implementation of a decoder assuming cross attention
+    """
     def __init__(self, d_k, d_model, d_v, d_ff, num_heads, num_layers, vocab_size, out_dims, dropout=0.1) -> None:
         super(Decoder, self).__init__()
         self.d_model = d_model
