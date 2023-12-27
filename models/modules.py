3cff646fd3b2e0921dc413f4f47a5940b9eeb28c
aryamanpandya99 <aryaman.pandya@tufts.edu>
2023-12-26 21:17:06 -0500

    modified:   models/modules.py 	modified:   notebooks/decoder_only_gpt.ipynb

--- models/modules.py
+++ models/modules.py
@@ -8,6 +8,7 @@ from torch import nn
 def scaled_dot_product_attention(q, k, d_k, mask):
     scores = torch.matmul(q, k.transpose(-2, -1)) / math.sqrt(d_k)
     if mask is True:
+        mask = torch.tril(torch.ones(scores.shape))
         scores = scores.masked_fill(mask == 0, float('-inf'))
     return nn.Softmax(-1)(scores)
 
